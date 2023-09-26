8ac9a5886fcbea47e810ae5a50fea4cdec4608bf
Kai Vandivier <kpvandivier@gmail.com>
2023-09-26 17:19:34 +0200

    feat: multiple entry points with plugin.html

--- src/Plugin.js
+++ src/Plugin.js
@@ -1,1 +1,39 @@
-// placeholder
+// This file is modified for this Vite demo
+
+import AppAdapter from '@dhis2/app-adapter'
+import { Layer, layers, CenteredContent, CircularLoader } from '@dhis2/ui'
+import React from 'react'
+
+// * Import Spinner instead of app
+
+const D2App = React.lazy(() =>
+    import(/*webpackChunkName: 'app'*/ './D2App/components/Spinner/Spinner.js')
+) // Automatic bundle splitting!
+
+const appConfig = {
+    url: process.env.REACT_APP_DHIS2_BASE_URL,
+    appName: process.env.REACT_APP_DHIS2_APP_NAME || '',
+    appVersion: process.env.REACT_APP_DHIS2_APP_VERSION || '',
+    apiVersion: parseInt(process.env.REACT_APP_DHIS2_API_VERSION),
+    pwaEnabled: process.env.REACT_APP_DHIS2_APP_PWA_ENABLED === 'true',
+    // Hardcode this for vite demo:
+    plugin: true // process.env.REACT_APP_DHIS2_APP_PLUGIN === 'true',
+}
+
+const App = () => (
+    <AppAdapter {...appConfig}>
+        <React.Suspense
+            fallback={
+                <Layer level={layers.alert}>
+                    <CenteredContent>
+                        <CircularLoader />
+                    </CenteredContent>
+                </Layer>
+            }
+        >
+            <D2App config={appConfig} />
+        </React.Suspense>
+    </AppAdapter>
+)
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -22,6 +23,16 @@ export default defineConfig(({ mode }) => {
     // Need to add vars on process.env here
     define: {
       "process.env": JSON.stringify(env),
+    },
+
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          // TODO: Make dynamic
+          plugin: resolve(__dirname, 'plugin.html'),
+        }
+      }
     },
 
     // Allow JSX in .js files (at the cost of performance):

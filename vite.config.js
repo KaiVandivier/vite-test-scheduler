513856311bb19370c2e6e64db88d15fb62170337
Kai Vandivier <kpvandivier@gmail.com>
2024-01-15 12:03:58 +0100

    feat: config for plugins and SW

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { resolve } from 'path'
+import { resolve } from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,6 +6,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   // https://vitejs.dev/config/#using-environment-variables-in-config
   const env = loadEnv(mode, process.cwd(), ["REACT_APP", "NODE_ENV"]);
+
+  // WIP
+  // Use individual properties for drop-in replacements instead of a whole
+  // object, which allows for better code trimming optimizations
+  const defineOptions = {};
+  Object.entries(env).forEach(([key, val]) => {
+    defineOptions[`process.env.${key}`] = JSON.stringify(val);
+  });
+  console.log({ env, defineOptions });
 
   return {
     plugins: [react()],
@@ -20,19 +29,28 @@ export default defineConfig(({ mode }) => {
     // https://vitejs.dev/config/shared-options.html#envprefix
     envPrefix: "REACT_APP",
 
-    // Need to add vars on process.env here
-    define: {
-      "process.env": JSON.stringify(env),
-    },
+    // Need to add vars on process.env here -- drop-in replacement
+    define: defineOptions,
 
     build: {
       rollupOptions: {
         input: {
-          main: resolve(__dirname, 'index.html'),
-          // TODO: Make dynamic
-          plugin: resolve(__dirname, 'plugin.html'),
-        }
-      }
+          main: resolve(__dirname, "index.html"),
+          // Build an optional plugin -- shares code with main app
+          // TODO: Make dynamic, based on context
+          plugin: resolve(__dirname, "plugin.html"),
+          // Build the service worker
+          // (uses ES modules tho, which is incompatible with the previous FF ESR)
+          "service-worker": resolve(__dirname, "src/service-worker.js"),
+        },
+        output: {
+          // Make sure the service worker output file has the right name
+          entryFileNames: (assetInfo) =>
+            assetInfo.name === "service-worker"
+              ? "[name].js"
+              : "assets/[name]-[hash].js",
+        },
+      },
     },
 
     // Allow JSX in .js files (at the cost of performance):
